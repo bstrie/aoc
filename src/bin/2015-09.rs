f47e3776f8ca22ca2eb321a73bec83015062aba6
bstrie <865233+bstrie@users.noreply.github.com>
2021-05-22 23:43:59 -0400

    Fix formatting

--- src/bin/2015-09.rs
+++ src/bin/2015-09.rs
@@ -2,7 +2,7 @@
 
 use std::{
     cmp::{max, min},
-    collections::{HashMap, HashSet}
+    collections::{HashMap, HashSet},
 };
 
 fn main() {
@@ -12,73 +12,67 @@ fn main() {
 }
 
 fn part1(input: &str) -> i32 {
-	let mut places = Places::new(input, min);
-	places.permute(i32::MAX, places.names.len())
+    let mut places = Places::new(input, min);
+    places.permute(i32::MAX, places.names.len())
 }
 
 fn part2(input: &str) -> i32 {
-	let mut places = Places::new(input, max);
-	places.permute(0, places.names.len())
+    let mut places = Places::new(input, max);
+    places.permute(0, places.names.len())
 }
 
 struct Places<'a, F> {
-	names: Vec<&'a str>,
-	distances: HashMap<(&'a str, &'a str), i32>,
-    compare: F
+    names: Vec<&'a str>,
+    distances: HashMap<(&'a str, &'a str), i32>,
+    compare: F,
 }
 
-impl<'a, F: Fn(i32,i32)->i32> Places<'a, F> {
-	fn new(input: &str, compare: F) -> Places<F> {
-		let mut names = HashSet::new();
-		let mut distances = HashMap::new();
-		for line in input.lines() {
-			let parts: Vec<_> = line.split(' ').collect();
-			names.insert(parts[0]);
-			names.insert(parts[2]);
-			distances.insert(
-				if parts[0] < parts[2] {
-					(parts[0], parts[2])
-				} else {
-					(parts[2], parts[0])
-				},
-				parts[4].parse().unwrap()
-			);
-		}
-		Places {
-			names: names.into_iter().collect(),
-			distances,
-			compare
-		}
-	}
+impl<'a, F: Fn(i32, i32) -> i32> Places<'a, F> {
+    fn new(input: &str, compare: F) -> Places<F> {
+        let mut names = HashSet::new();
+        let mut distances = HashMap::new();
+        for line in input.lines() {
+            let parts: Vec<_> = line.split(' ').collect();
+            names.insert(parts[0]);
+            names.insert(parts[2]);
+            distances.insert(
+                if parts[0] < parts[2] {
+                    (parts[0], parts[2])
+                } else {
+                    (parts[2], parts[0])
+                },
+                parts[4].parse().unwrap(),
+            );
+        }
+        Places {
+            names: names.into_iter().collect(),
+            distances,
+            compare,
+        }
+    }
 
-	fn permute(&mut self, mut dist: i32, j: usize) -> i32 {
-		if j == 1 {
-			let mut total = 0;
-			for &[p1, p2] in self.names.array_windows() {
-                total += self.distances[&
-                    if p1 < p2 {
-					    (p1, p2)
-                    } else {
-					    (p2, p1)
-                    }
-                ];
-			}
-			return total;
-		}
+    fn permute(&mut self, mut dist: i32, j: usize) -> i32 {
+        if j == 1 {
+            let mut total = 0;
+            for &[p1, p2] in self.names.array_windows() {
+                total += self.distances[&if p1 < p2 { (p1, p2) } else { (p2, p1) }];
+            }
+            return total;
+        }
 
-		for i in 0..j {
-            let newdist = self.permute(dist, j-1);
-			dist = (self.compare)(dist, newdist);
+        for i in 0..j {
+            let newdist = self.permute(dist, j - 1);
+            dist = (self.compare)(dist, newdist);
 
-			if j % 2 == 0 {
-				self.names.swap(i, j-1);
-			} else {
-				self.names.swap(0, j-1);
-			}
-		}
+            if j % 2 == 0 {
+                self.names.swap(i, j - 1);
+            } else {
+                self.names.swap(0, j - 1);
+            }
+        }
 
         dist
-	}
+    }
 }
 
 #[test]
